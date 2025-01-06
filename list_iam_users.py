361e3494306c9f29ea171ccce0ca3c756d9f9933
nexus <emekaezeobi@outlook.com>
2025-01-06 22:42:53 +0100

    spell check comment

--- list_iam_users.py
+++ list_iam_users.py
@@ -1,5 +1,5 @@
 # Import all the modules and Libraries
-import boto3
+import boto3 # type: ignore
 # Open Management Console with "default" as the user created using the AWS configure command
 aws_management_console = boto3.session.Session(profile_name="default")
 # Open IAM console
@@ -8,3 +8,5 @@ iam_console = aws_management_console.client(service_name="iam")
 iam_users = iam_console.list_users()
 for each_iam_user in iam_users['Users']:
     print(each_iam_user['UserName'])
+
+    

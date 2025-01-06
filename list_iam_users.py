11968823a210e39d2b09caf9340dca12c8faa952
nexus <emekaezeobi@outlook.com>
2025-01-06 21:42:18 +0100

    Added documentation link as a comment to the code

--- list_iam_users.py
+++ list_iam_users.py
@@ -4,6 +4,7 @@ import boto3
 aws_management_console = boto3.session.Session(profile_name="default")
 # Open IAM console
 iam_console = aws_management_console.client(service_name="iam")
+ # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
 iam_users = iam_console.list_users()
 for each_iam_user in iam_users['Users']:
     print(each_iam_user['UserName'])

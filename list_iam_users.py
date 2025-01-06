ec58055b1e0e4b4b950ebe1170c0b6ef85afd374
nexus <emekaezeobi@outlook.com>
2025-01-06 22:50:29 +0100

    added version 2 to the code

--- list_iam_users.py
+++ list_iam_users.py
@@ -1,12 +1,20 @@
-# Import all the modules and Libraries
-import boto3 # type: ignore
-# Open Management Console with "default" as the user created using the AWS configure command
+# # Import all the modules and Libraries
+# import boto3 # type: ignore
+# # Open Management Console with "default" as the user created using the AWS configure command
+# aws_management_console = boto3.session.Session(profile_name="default")
+# # Open IAM console
+# iam_console = aws_management_console.client(service_name="iam")
+#  # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
+# iam_users = iam_console.list_users()
+# for each_iam_user in iam_users['Users']:
+#     print(each_iam_user['UserName'])
+
+
+import boto3
 aws_management_console = boto3.session.Session(profile_name="default")
-# Open IAM console
-iam_console = aws_management_console.client(service_name="iam")
- # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
-iam_users = iam_console.list_users()
-for each_iam_user in iam_users['Users']:
-    print(each_iam_user['UserName'])
+iam_console = aws_management_console.resource('iam')
+
+for each_user in iam_console.users.all():
+    print(each_user.name)
 
     
